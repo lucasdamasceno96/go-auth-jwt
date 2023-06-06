44d1867039c7f387e181a3517c42a3180b5f1790
Lucas Damasceno <lucasdamascenopinto96@gmail.com>
2023-06-05 21:26:46 -0300

    fix: connected postgres using docker instead of elephant.sql

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/lucasdamasceno96/go-auth-jwt.git/initializers"
 	"github.com/lucasdamasceno96/go-auth-jwt.git/models"
 	"golang.org/x/crypto/bcrypt"
@@ -45,5 +48,60 @@ func SignUp(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{
 		"User" : "Succesfully created",
 	})
+
+}
+
+
+func Login (c *gin.Context) {
+	var body struct {
+		Email string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error" : "failed to read the body",
+		})
+		return
+	}
+
+	var user models.User
+	initializers.DB.First(&user, "email= ?", body.Email)
+
+	if user.ID == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+			"error" : "Invalid Email or Password.",
+		})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+			"error" : "Invalid Email or Password.",
+		})
+		return 
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+			"error" : "Failed to create token",
+		})
+		return 
+	}
+
+	c.JSON(http.StatusOK, gin.H {
+		"token": tokenString,
+	})
+
+
 
 }
--- main.go
+++ main.go
@@ -16,5 +16,6 @@ func main() {
 	
   r := gin.Default()
   r.POST("/signup", controllers.SignUp)
+    r.POST("/login", controllers.Login)
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
